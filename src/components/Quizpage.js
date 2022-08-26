b39aa1fc6f6b1b838abc24387ada886fe95e146f
naaabee <naaabee@naaabees-MacBook-Air.local>
2022-08-26 16:48:41 +0800

    quiz page styling complete

--- src/components/Quizpage.js
+++ src/components/Quizpage.js
@@ -1,35 +1,32 @@
 import React from 'react'
 import { quiz } from '../quizDatabase'
+import './Quizpage.css'
 
 export default function Quizpage(props) {
 
     const quizzes = quiz.results
     const questions = quizzes.map(quiz => {
         return (
-            <>
+            <div className='question--tab'>
                 <h4>{quiz.question}</h4>
-                <ul>
+                <ul className='question--list'>
                     <li>{quiz.correct_answer}</li>
                     <li>1</li>
                     <li>3</li>
                     <li>5</li>
                 </ul>
-            </>
+                <hr></hr>
+            </div>
         )
     })
 
-    // For answers can create an empty array, 
-
-
-
     return (
-        <>
-
-            <div>
+        <div className='question--section'>
+            <div className='question--container'>
                 {questions}
             </div>
-
-        </>
+            <button className='question--button'>Check answers</button>
+        </div>
 
     )
 }
